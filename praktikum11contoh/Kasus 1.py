6c957033091f61d12454b5949c5d40e61e80df8f
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2020-12-30 23:51:32 +0700

    update

--- praktikum11contoh/Kasus 1.py
+++
@@ -1,8 +0,0 @@
-L=[]
-while True:
-    l=int(input())
-    if l == 0:
-        break
-    else:
-        L.append(l)  
-print(L)
--- praktikum11contoh/Kasus 3.py
+++
@@ -1,9 +0,0 @@
-def count(val):
-    return sum(i % 2 == 1 for i in val)
-
-def main():
-    val = list(map(int, input().split()))
-    print(count(val))
-
-if __name__ == "__main__":
-    main()
